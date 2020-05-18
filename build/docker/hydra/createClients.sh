bb3e242369ef46ea555257aaccfcf0c919122693
Tran Toan Van <trantoanvan@outlook.com>
2020-05-18 14:40:56 +0700

    split Hydra into multiple compose files

--- build/docker/hydra/createClients.sh
+++ build/docker/hydra/createClients.sh
@@ -1,50 +1,39 @@
 #!/bin/bash
 
-export CALLBACK_URL=http://10.254.254.254:9010/callback
+export HYDRA_IMG=oryd/hydra:v1.3
 
-docker run --rm -it \
-    -e HYDRA_ADMIN_URL=http://10.254.254.254:4445 \
-    oryd/hydra:v1.3.2-alpine \
-        clients create --skip-tls-verify \
-            --id client1 \
-            --secret some-secret \
-            --grant-types authorization_code,refresh_token,client_credentials,implicit \
-            --response-types token,code,id_token \
-            --scope openid,offline,photos.read \
-            --callbacks ${CALLBACK_URL}
+docker run --rm -it --network=https-proxy ${HYDRA_IMG} \
+      clients create \
+        --fake-tls-termination --skip-tls-verify \
+        --endpoint "http://hydra:4445" \
+        --id theleague \
+        --name "TheLeague Client" \
+        --secret some-secret \
+        --token-endpoint-auth-method client_secret_post \
+        --grant-types authorization_code,refresh_token,implicit \
+        --response-types token,code,id_token,refresh_token \
+        --scope openid,offline,offline_access,account.profile,account.read,photos.read \
+        --callbacks "https://id.dev.mio/connect/hydra/check"
 
-docker run --rm -it \
-	oryd/hydra:v1.3.2-alpine \
-		clients create --skip-tls-verify \
-			--endpoint http://10.254.254.254:4445 \
-			--id client2 \
-			--secret some-secret \
-			--grant-types authorization_code,refresh_token,client_credentials,implicit \
-			--response-types token,code,id_token \
-			--scope openid,offline,photos.read \
-			--callbacks ${CALLBACK_URL}
+docker run --rm -it --network=https-proxy ${HYDRA_IMG} \
+      clients create \
+        --fake-tls-termination --skip-tls-verify \
+        --endpoint "http://hydra:4445" \
+        --id oauthdebugger \
+        --name "OAuthDebugger Client" \
+        --secret some-secret \
+        --token-endpoint-auth-method client_secret_basic \
+        --grant-types authorization_code,refresh_token,implicit \
+        --response-types token,code,id_token,refresh_token \
+        --scope openid,offline,offline_access,account.profile,account.read,photos.read \
+        --callbacks https://oauthdebugger.com/debug
 
-
-docker run --rm -it \
-  --network=https-proxy \
-	oryd/hydra:v1.3.2-alpine \
-		clients create --skip-tls-verify \
-			--endpoint http://hydra:4445 \
-			--id client3 \
-			--secret some-secret \
-			--grant-types authorization_code,refresh_token,client_credentials,implicit \
-			--response-types token,code,id_token \
-			--scope openid,offline,photos.read \
-			--callbacks ${CALLBACK_URL}
-
-docker run --rm -it \
-  --network=https-proxy \
-	oryd/hydra:v1.3.2-alpine \
-		clients create --skip-tls-verify \
-			--endpoint http://hydra:4445 \
-			--id oauthdebugger \
-			--secret some-secret \
-			--grant-types authorization_code,refresh_token,client_credentials,implicit \
-			--response-types token,code,id_token \
-			--scope openid,offline,photos.read \
-			--callbacks https://oauthdebugger.com/debug
+docker run --rm -it --network=https-proxy ${HYDRA_IMG} \
+      clients create \
+        --fake-tls-termination --skip-tls-verify \
+        --endpoint "http://hydra:4445" \
+        --id machine \
+        --secret some-secret \
+        --token-endpoint-auth-method client_secret_post \
+        --grant-types client_credentials,refresh_token \
+        --response-types token,id_token,refresh_token
